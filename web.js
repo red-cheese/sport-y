e731206b07f1a1d38b2ecc71a99b5cfa42c3aaaa
Alexandra Mikhaylova <mikhaylova.alexandra.a@gmail.com>
2014-05-25 16:16:31 +0400

    Singleton mongoClient

--- web.js
+++ web.js
@@ -60,8 +60,7 @@ var CATEGORIES_EN = {
 
 // Configuration
 
-var MongoClient = require('mongodb').MongoClient, Server = require('mongodb').Server;
-var mongoClient = new MongoClient(new Server('localhost', 27017));
+var MONGODB_URI = "mongodb://localhost/sporty-db", db, checkpoints;
 
 var app = express();
 app.use(express.bodyParser());
@@ -125,21 +124,15 @@ function nearest(items, lat, lng) {
 }
 
 function findCheckpoints(categories, lat, lng, lang, res) { // Ugly but works in the asynchronous Jaavascript nature
-    mongoClient.open(function(err, mongoClient) {
-	var db = mongoClient.db("sporty-db");
-	var coll = db.collection("checkpoints");
-	if (categories.length == 0) {
-	    coll.find().toArray(function(err, items) {
-		mongoClient.close();
-		res.render('index.ejs', { locals: { lat: lat, lng: lng, toDisplay: JSON.stringify(nearest(items, lat, lng)), lang: lang } });
-	    });
-	} else {
-	    coll.find({ category: { $in: categories } }).toArray(function(err, items) {
-		mongoClient.close();
-		res.render('index.ejs', { locals: { lat: lat, lng: lng, toDisplay: JSON.stringify(nearest(items, lat, lng)), lang: lang } });
-	    });
-	}
-    });
+    if (categories.length == 0) {
+	checkpoints.find().toArray(function(err, items) {
+	    res.render('index.ejs', { locals: { lat: lat, lng: lng, toDisplay: JSON.stringify(nearest(items, lat, lng)), lang: lang } });
+	});
+    } else {
+	checkpoints.find({ category: { $in: categories } }).toArray(function(err, items) {
+	    res.render('index.ejs', { locals: { lat: lat, lng: lng, toDisplay: JSON.stringify(nearest(items, lat, lng)), lang: lang } });
+	});
+    }
 }
 
 app.post('/sport-y/', function(req, res) {
@@ -168,6 +161,11 @@ app.post('/sport-y/en', function(req, res) {
 
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-    });
+mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
+	if (err) throw err;
+	db = database;
+	checkpoints = db.collection("checkpoints");
+	app.listen(port, function() {
+		console.log("Listening on " + port);
+	});
+});
